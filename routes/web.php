7eedbcdc017a84eeb541b50213fe450c560d1dc4
Ralph Schindler <ralph@ralphschindler.com>
2020-09-24 09:47:11 -0500

    publish livewire assets

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/', function () {
 Route::get('/debug', function () {
     dump('app config', config('app'));
     dump('livewire config ', config('livewire'));
+
+    unset($_ENV['AWS_ACCESS_KEY_ID'], $_ENV['AWS_SECRET_ACCESS_KEY'], $_ENV['AWS_SESSION_TOKEN']);
+
     dump('_ENV', $_ENV);
     dump('Livewire Scripts', \Livewire::scripts());
 });
